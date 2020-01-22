ee762768af9707379162ae8c6efca78de6b2bf91
Mark Bernstein <mberns006@gmail.com>
2020-01-21 17:23:08 -0800

    changed database

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection;
+var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.send.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     port: 3306,
-//     user: 'rw0r6n2rfyde7biu',
-//     password: 'ei51yzta7qxt0n8w',
-//     database: 'uylkyjc9hkq1fcvy'
-//   })
-// }
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.send.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'bjrz09yojl1nxyd7',
+    password: 'f28bv5hv1azqz9jl',
+    database: 'jicvb1qcegchcifs'
+  })
+}
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Maekownz22!",
-  database: "genre_database"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Maekownz22!",
+//   database: "genre_database"
+// });
 
 // Make connection.
 connection.connect(function(err) {
